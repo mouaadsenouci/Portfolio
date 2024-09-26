be738e1cb1dd86647109be9ae3a59cbb4a31a422
Mouaad Senouci <157650943+mouaadsenouci@users.noreply.github.com>
2024-09-26 17:01:44 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Programmer", "Web Developer", "Front-End and Back-End developer", "Youtuber"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
